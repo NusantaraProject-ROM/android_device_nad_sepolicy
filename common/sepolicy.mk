268fd21715dcb0a2ed0f7da6d0fc507ba7202788
Genkzsz11 <genkzsz11@gmail.com>
2019-09-20 19:32:35 +0700

    Init DU
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/du/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/du/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/du/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -4,8 +4,8 @@
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/du/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/du/sepolicy/qcom/common \
+    device/du/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
