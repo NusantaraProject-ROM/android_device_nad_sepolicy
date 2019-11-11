5b17eb470d26fca2d41aee30e2d2c1fad007ee41
Danny Baumann <dannybaumann@web.de>
2019-11-11 11:54:29 +0100

    qcom: Add legacymm livedisplay sepolicy
    Change-Id: Ic8e243e18cb9acf842d7afd9af0cecbf4550b1ae

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -20,3 +20,8 @@ BOARD_SEPOLICY_DIRS += \
     device/nad/sepolicy/qcom/dynamic \
     device/nad/sepolicy/qcom/vendor
 endif
+
+ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+BOARD_SEPOLICY_DIRS += \
+    device/nad/sepolicy/qcom/legacy-vendor
+endif
